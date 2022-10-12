30db68dd3e39e1e4705da15a27b3f9a09c46c653
gobindacb <p.gobinda.cb@gmail.com>
2022-10-12 22:54:09 +0600

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <footer className="px-4 py-8 bg-gray-300 text-pink-400">
+            <footer className="px-4 py-8 bg-gray-400 text-pink-700">
                 <div className="container flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
                     <div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
                         <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 rounded-full dark:bg-violet-400">
